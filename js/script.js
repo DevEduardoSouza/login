9ad7f70a79abe813f63fbc734eceff4efde0f5e0
Eduardo <eduardo456.acc38@gmail.com>
2023-01-20 11:01:36 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let  btnEntrar =  document.querySelector("#btn_1").addEventListener('click', ()=>{
+
+    document.querySelector("#emailInput").value;
+    console.log( document.querySelector("#emailInput").value );
+});
